da3d6fd5b011f4ff9771289254d385fa7c283f45
John Nelson <john@mikelopezuniversity.ml>
2021-03-03 23:18:38 -0600

    Add multiple roots and sign up button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "welcome#show"
-
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: 'recipes#index', as: :signed_in_root
+  end
+  constraints Clearance::Constraints::SignedOut.new do
+    root to: 'welcome#show'
+  end
   resources :recipes
   resources :meal_plans
 end
